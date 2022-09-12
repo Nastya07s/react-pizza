1bce490c54d07c469556a138a8b945953207dadf
Nastya07s <nastya.kotovich07@gmail.com>
2022-09-12 16:01:17 +0200

    fix: skeleton

--- src/components/PizzaBlock/Skeleton.jsx
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -11,10 +11,10 @@ const Skeleton = (props) => (
     foregroundColor="#ecebeb"
     {...props}>
     <circle cx="125" cy="125" r="125" />
-    <rect x="0" y="263" rx="5" ry="5" width="250" height="20" />
-    <rect x="128" y="403" rx="30" ry="30" width="120" height="46" />
-    <rect x="0" y="303" rx="5" ry="5" width="250" height="83" />
-    <rect x="1" y="416" rx="5" ry="5" width="100" height="25" />
+    <rect x="0" y="293" rx="5" ry="5" width="250" height="20" />
+    <rect x="128" y="433" rx="30" ry="30" width="120" height="46" />
+    <rect x="0" y="333" rx="5" ry="5" width="250" height="83" />
+    <rect x="1" y="446" rx="5" ry="5" width="100" height="25" />
   </ContentLoader>
 );
 
